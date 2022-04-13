4bac9d486be2232f4b2659623f22bfe6b679fced
Коньков Владислав <v.konkov@bftcom.com>
2022-04-13 23:06:19 +0500

    Revert "Решение задания"
    This reverts commit 8324b397f62a8aadbdbc35eee6946ccdb2b86684.

--- src/Cycles.js
+++ src/Cycles.js
@@ -3,43 +3,18 @@
   Используя цикл for, просуммируйте все четные числа в диапазоне между этими значениями (включительно)
   и верните итоговый результат.
 */
-export function rangeSum(start, end) {
-    let sum = 0;
-    for (let i = start; i <= end; i++) {
-        if (i % 2 === 0) {
-            sum += i;
-        }
-    }
-    return sum;
-}
+export function rangeSum(start, end) {}
 
 /*
   В функцию iterationCount() приходит неотрицательное число.
   Используя цикл while, выполняйте деление этого числа на два до тех пор, пока результат деления больше 0.1
   и верните количество потребовавшихся итераций (т.е. сколько раз пришлось выполнить деление).
 */
-export function iterationCount(a) {
-    let iterationCount = 0;
-    while (a > 0.1) {
-        a /= 2.0;
-        iterationCount++;
-    }
-    return iterationCount;
-}
+export function iterationCount(a) {}
 
 /*
   В функцию symbolsReplace() приходит строка текста.
   Используя цикл do while, замените в тексте каждый третий символ на символ нижнего подчеркивания
   и верните итоговый результат.
 */
-export function symbolsReplace(message) {
-    let count = 0;
-    let result = message;
-    do {
-        if ((count + 1) % 3 === 0) {
-            result = result.slice(0, count) + '_' + result.slice(count + 1);
-        }
-        count++;
-    } while (count < message.length);
-    return result;
-}
+export function symbolsReplace(message) {}
